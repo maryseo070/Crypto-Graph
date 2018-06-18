456ece220a66a5a5c4a4cba852ad6e3ed006d451
maryseo070 <maryseo070@gmail.com>
2018-06-18 14:44:57 -0700

    removes excess axes files

---
+++ src/components/month/DataVis.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+// import { axis } from 'd3-axis';
+import './DataVis.css';
+import * as d3 from 'd3';
+import { scaleLinear,scaleTime } from 'd3-scale';
+import { axisBottom, axisLeft } from 'd3-axis';
+import { line } from 'd3-shape';
+import { select } from 'd3-selection';
+import * as d3Axis from 'd3-axis';
+
+const size = 500;
+const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+const width = 960 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+const x = scaleTime()
+  .domain([new Date(2001, 0, 1)], new Date(2006, 0, 1))
+  .range(([0, width]));
+
+const y = scaleLinear()
+  .domain([0, 6])
+  .range([height, 0]);
+
+const xAxis = axisBottom(x);
+
+const yAxis = axisLeft(y);
+  // .scale(y);
+
+const connectingLine = line()
+  // .interpolate("monotone")
+  .x(function(d) { return x(d[0]); })
+  .y(function(d) { return y(d[1]); });
+
+
+
+
+
+const DataVis = ({ data, w, h }) => {
+  return(
+    <svg
+       width={width}
+       height={height}
+       viewBox={`0 0 ${size} ${size}`}
+       tranform={`translate(${margin.left}, ${margin.top})`}>
+       <g
+          className="x-axis"
+          tranform={`translate(0, ${height})`}>
+          {axisBottom(x)}
+        </g>
+     </svg>
+
+  );
+  // return(
+  //   <svg
+  //     width={width}
+  //     height={height}
+  //     viewBox={`0 0 ${size} ${size}`}
+  //     tranform={`translate(${margin.left}, ${margin.top})`}>
+  //
+  //     <g
+  //       className="x-axis"
+  //       tranform={`translate(0, ${height})`}>
+  //       {xAxis(height)}
+  //     </g>
+  //
+  //     <g
+  //       className="y-axis"
+  //       call={yAxis}>
+  //     </g>
+  //
+  //     <path
+  //       className="line"
+  //       d={connectingLine}>
+  //     </path>
+  //
+  //     <circle
+  //       className="dot"
+  //       cx={connectingLine.x()}
+  //       cy={connectingLine.y()}
+  //       r="3.5"
+  //       >
+  //
+  //     </circle>
+  //
+  //   </svg>
+  // );
+};
+
+export default DataVis;
--- src/components/month/eth_month.jsx
+++ src/components/month/eth_month.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import * as d3 from "d3";
 import PieChart from './PieChart.jsx';
+import DataVis from './DataVis.jsx';
+import './eth_month.css';
 
 class ETHMonth extends Component {
   constructor(props) {
@@ -15,6 +16,14 @@ class ETHMonth extends Component {
       { label: '25-44', value: 14106543 },
       { label: '45-64', value: 8819342 },
       { label: '≥65', value: 612463 },
+    ],
+    datavis: [
+      [new Date(2001, 0, 1), 1],
+      [new Date(2002, 0, 1), 2],
+      [new Date(2003, 0, 1), 2],
+      [new Date(2004, 0, 1), 3],
+      [new Date(2005, 0, 1), 4],
+      [new Date(2006, 0, 1), 5]
     ]
   };
     this.showETHdata = this.showETHdata.bind(this);
@@ -28,13 +37,19 @@ class ETHMonth extends Component {
     const containerWidth = '200px';
     const containerHeight = "200px";
     const data = this.state.data;
+    const data2 = this.state.datavis;
     return (
-      <div>
+      <div className="wrapper-div">
         <PieChart
           data={data}
           width={containerWidth}
           height={containerHeight}
         />
+        <DataVis
+          data={data2}
+          width={containerWidth}
+          height={containerHeight}
+          />
       </div>
     );
   }
