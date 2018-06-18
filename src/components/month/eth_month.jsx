7602d88fedd557af12350617dfd5d37fcf348f79
maryseo070 <maryseo070@gmail.com>
2018-06-17 22:09:10 -0700

    starts d3

--- src/components/month/eth_month.jsx
+++ src/components/month/eth_month.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import * as d3 from "d3";
+
 
 class ETHMonth extends Component {
   constructor(props) {
@@ -12,18 +14,27 @@ class ETHMonth extends Component {
   }
 
   showETHdata() {
-    let eth = this.props.eth;
-    if (eth.length > 0) {
-      return eth.map( (data) => (
-        <ul>
-          <li>{data.time}</li>
-          <li>{data.close}</li>
-          <li>{data.high}</li>
-          <li>{data.low}</li>
-          <li>{data.open}</li>
-        </ul>
-      ))
-    }
+    // let eth = this.props.eth;
+    // if (eth.length > 0) {
+    //   return eth.map( (data, i) => (
+    //     <ul key={i}>
+    //       <li>{data.time}</li>
+    //       <li>{data.close}</li>
+    //       <li>{data.high}</li>
+    //       <li>{data.low}</li>
+    //       <li>{data.open}</li>
+    //     </ul>
+    //   ));
+    // }
+
+    var svg = d3.select("body").append("svg")
+      .attr("height", 200)
+      .attr("width", 200);
+    var circles = d3.selectAll("circle")
+      .data({"x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green"});
+
+    return circles;
+
   }
 
   render() {
